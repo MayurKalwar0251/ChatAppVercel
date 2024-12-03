67a48f53ebc78d5a163901bc85e573f9e543400f
Mayur Kalwar <mayurkalwar0251@gmail.com>
2024-12-03 13:46:22 +0530

    Connection uri issue

--- backend/utils/connectDb.js
+++ backend/utils/connectDb.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    const connect = await mongoose.connect(process.env.DB);
+    const connect = await mongoose.connect(
+      "mongodb+srv://mayurkalwar0251:znzl8kXYQzBZP6W8@cluster0.gszow.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log(`MongoDB connected with server: ${connect.connection.host}`);
   } catch (error) {
     console.log(error);
